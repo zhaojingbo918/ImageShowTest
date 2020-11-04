8ba0122c732eab9dd6e95e5697e67f7ed0b5d11f
jingbo <jingbo@nolovr.com>
2020-11-04 09:59:28 +0800

    init repository

---
+++ ImageShowTest/ImageBuilder.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace ImageShowTest
+{
+    public class ImageBuilder
+    {
+        [DllImport("gdi32")]
+        static extern int DeleteObject(IntPtr o);
+        /// <summary>
+        /// 创建有线图标
+        /// </summary>
+        /// <param name="yxmodel"></param>
+        /// <returns></returns>
+        public ImageSource CreateImage()
+        {
+            Bitmap bmp = new Bitmap(150, 150);
+            Graphics graphics = Graphics.FromImage(bmp);
+            int targetX, targetY, srcX, srcY, width, height;
+            Rectangle rect1;
+
+            // graphics.Clear(System.Drawing.Color.Transparent);
+            Random random = new Random();
+
+            System.Drawing.Pen pen = new System.Drawing.Pen(System.Drawing.Brushes.Red);
+
+            for (int i = 0; i < 600; i++)
+            {
+                graphics.DrawRectangle(pen, new Rectangle(random.Next(150), random.Next(150), 1, 1));
+            }
+
+
+            graphics.Dispose();
+            IntPtr it = bmp.GetHbitmap();
+            BitmapSource bitmapSource = Imaging.CreateBitmapSourceFromHBitmap(it, IntPtr.Zero,
+                                                                                                     Int32Rect.Empty,
+                                                                                                     BitmapSizeOptions.FromEmptyOptions
+                                                                                                         ());
+            DeleteObject(it);
+            bmp.Dispose();
+
+            return bitmapSource;
+        }
+    }
+}
---
+++ ImageShowTest/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace ImageShowTest
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ImageBuilder imageBuilder = new ImageBuilder();
+        DispatcherTimer m_Timer;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+            m_Timer = new DispatcherTimer() { Interval = TimeSpan.FromMilliseconds(5) };
+            m_Timer.Tick += M_Timer_Tick;
+            m_Timer.Start();
+        }
+
+        private void M_Timer_Tick(object sender, EventArgs e)
+        {
+            this.image.Source = imageBuilder.CreateImage();
+        }
+    }
+}
